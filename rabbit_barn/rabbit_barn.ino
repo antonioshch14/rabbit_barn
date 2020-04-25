263dc501ec4747f872b475de1e30a2d9a085966b
Anton Belov <antonioshch14@gmail.com>
2020-04-25 12:44:38 +0300

    Add project files.

---
+++ rabbit_barn/rabbit_barn.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		rabbit_barn.ino
+ Created:	2020-04-25 12:44:13
+ Author:	z003pr4u
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
